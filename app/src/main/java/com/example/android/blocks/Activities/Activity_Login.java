47c0624d0c709a9457f3a637efe8fd868fb9c0ba
Sebassllr <sebaslunasllr@gmail.com>
2017-11-28 10:12:03 -0500

    Proyecto

---
+++ app/src/main/java/com/example/android/blocks/Activities/Activity_Login.java
@@ -0,0 +1,15 @@
+package com.example.android.blocks.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.blocks.R;
+
+public class Activity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__login);
+    }
+}
---
+++ app/src/main/java/com/example/android/blocks/Activities/Activity_Register.java
@@ -0,0 +1,15 @@
+package com.example.android.blocks.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.blocks.R;
+
+public class Activity_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__register);
+    }
+}
